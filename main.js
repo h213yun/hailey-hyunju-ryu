3d6b9f0e003f4fbef0fd174c963dcf88535d419f
cvlab <rhyunju31@gmail.com>
2026-05-02 19:35:14 +0900

    Update features

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
 var mobileMenu = document.querySelector(".mobile-menu");
+var lastUpdated = document.querySelector("#last-updated");
 
-mobileMenuBtn.addEventListener("click", () => {
-  if (mobileMenu.style.display === "none") {
-    mobileMenu.style.display = "flex";
-    mobileMenuBtn.innerHTML = "Close";
-  } 
-  else {
-    mobileMenu.style.display = "none";
-    mobileMenuBtn.innerHTML = "Menu";
+if (mobileMenuBtn && mobileMenu) {
+  mobileMenuBtn.addEventListener("click", () => {
+    if (mobileMenu.style.display === "none") {
+      mobileMenu.style.display = "flex";
+      mobileMenuBtn.innerHTML = "Close";
+    } else {
+      mobileMenu.style.display = "none";
+      mobileMenuBtn.innerHTML = "Menu";
+    }
+  });
+}
+
+if (lastUpdated) {
+  var modifiedDate = new Date(document.lastModified);
+
+  if (!Number.isNaN(modifiedDate.getTime())) {
+    lastUpdated.textContent = modifiedDate.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
   }
-});
+}
